325e1b70eaba5ae91574a47d12ce57f610e15dfe
Giada92 <giada_meloni@virgilio.it>
2021-09-06 16:10:40 +0200

    add:footer

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    {{-- @dd($newRestaurant) --}}
+    {{-- @dd($newRestaurant->user->name) --}}
     {{-- @if (!$newRestaurant->isEmpty())
         @foreach ($newRestaurant as $item)
             <p>{{ $item->name }}</p>
@@ -11,33 +11,66 @@
         <p>Nesun Ristorante associato</p>
     @endif --}}
     @if ($newRestaurant)
-        <h2>{{ $newRestaurant->name }}</h2>
-        <img src="{{ asset('storage/' . $newRestaurant->cover) }}" alt="{{ $newRestaurant->name }}" style="width: 500px">
-        <p>{{ $newRestaurant->description }}</p>
-        {{-- @dd($newRestaurant->categories) --}}
-        <ul>
-            @foreach ($newRestaurant->categories as $item)
-                <li class="badge badge-pill badge-secondary">{{ $item->name }}</li>   
-            @endforeach
-        </ul>
+        <h2 class="title green">{{ $newRestaurant->name }}</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <img src="{{ asset('storage/' . $newRestaurant->cover) }}" alt="{{ $newRestaurant->name }}" style="width: 500px">
+                </div>
+                <div class="col">
+                    <h5><strong>Propietario:</strong> {{ $newRestaurant->user->name }}</h5>
+                    <h5><strong>Partita Iva:</strong> {{ $newRestaurant->piva }}</h5>
+                    <br>
+                    <h5><strong>Città:</strong> Roma</h5>
+                    <h5><strong>Via:</strong> {{ $newRestaurant->address }}</h5>
+                    <h5><strong>Partita Iva:</strong> {{ $newRestaurant->piva }}</h5>
 
-        <div class="my-3">
-            <a href="{{ route('admin.restaurants.edit', $newRestaurant->id) }}" class="btn btn-info">Modifica</a>
+                    <p class="category mt-3 mb-0"><strong>CATEGORIE:</strong></p>
+                    @foreach ($newRestaurant->categories as $item)
+                        <span class="btn-secondary btn-sm">{{ $item->name }}</span>   
+                    @endforeach
+                </div>
+            </div>
         </div>
-        <a href="{{ route('admin.plates.index') }}" class="btn btn-info">Visualizza il tuo Menù</a>
-        <form action="{{ route('admin.restaurants.destroy', $newRestaurant->id) }}" method="POST" onSubmit="return confirm('Sei sicuro di voler eliminare questo Ristorante?')">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">DELETE</button>
-        </form>
+        
+        <p class="p-3 text-justify">{{ $newRestaurant->description }}</p>
+        {{-- @dd($newRestaurant->categories) --}}
+
+        <div class="text-center wrapper_link">
+            <a href="{{ route('admin.restaurants.edit', $newRestaurant->id) }}" class="btn bg">Modifica Locale</a>
+            <a href="{{ route('admin.plates.index') }}" class="btn bg">Visualizza il Menù</a>
+            <a href="#" class="btn bg">I tuoi Ordini</a>
+            <form action="{{ route('admin.restaurants.destroy', $newRestaurant->id) }}" method="POST" onSubmit="return confirm('Sei sicuro di voler eliminare questo Ristorante?')">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="btn bg">DELETE</button>
+            </form>
+        </div>
+        
     @else
         <p>Non ci sono ristoranti</p>
-        <a href="{{ route('admin.restaurants.create') }}" class="btn btn-info">Crea</a>
+        <a href="{{ route('admin.restaurants.create') }}" class="btn">Crea</a>
     @endif
-    
-    
-    
-   
 </div>    
+
+<style>
+    .title {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    
+    .bg {
+        background-image: linear-gradient(90deg, #ee3c4a, #fc8237);
+        color: white;
+    }
+
+    form {
+        display: inline-block;
+    }
+
+    .wrapper_link > a {
+        margin: 0 10px;        
+    }
+</style>
     
 @endsection
