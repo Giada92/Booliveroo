2db239a38a437015e8c2ea293416929be621937d
DamianoTrifone <trifone.damiano@gmail.com>
2021-09-07 16:57:09 +0200

    Footer

--- public/js/front.js
+++ public/js/front.js
@@ -20007,8 +20007,7 @@ var router = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\windows\Desktop\laravel-qualcosa\resources\js\front.js */"./resources/js/front.js");
-
+module.exports = __webpack_require__(/*! C:\Users\damy1\boolean-php\laravel-qualcosa\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
